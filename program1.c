3f72fa861419fe4b9fc103ac19346fd7681c4e71
cavannis <chappellh@sou.edu>
2023-04-27 13:52:40 -0700

    4/27/2023 c practice with memory

---
+++ program1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    printf("Address of a in program1: %p\n", &a);
+    return 0;
+}
---
+++ program2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    int b = 10;
+    printf("Address of b in program2: %p\n", &b);
+    return 0;
+}
